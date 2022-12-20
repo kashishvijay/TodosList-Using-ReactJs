8635a62dcbe8052735c7523e281a5a79377f1a70
Kashish Vijay <kashishvijay2019@gmail.com>
2022-12-20 22:11:24 +0530

    Initial commit

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>this is about component
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi atque illum quisquam, alias, nam, error quibusdam dignissimos doloribus maiores mollitia quod fugiat? Molestias rem porro perspiciatis modi quibusdam facere voluptates. Natus, suscipit!</p>
+    </div> 
+  )
+
+}
